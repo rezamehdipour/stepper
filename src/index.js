6463942a7465dd7174da877e8848d23e7115a3e3
Reza Mehdipour <rezamehdipour@iran.ir>
2022-04-09 20:51:30 +0430

    little changes

--- src/index.js
+++ src/index.js
@@ -1,24 +1,24 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import { BrowserRouter } from 'react-router-dom';
+import React from "react";
+import ReactDOM from "react-dom";
+import { BrowserRouter } from "react-router-dom";
 
 // Redux
-import { Provider as ReduxProvider } from 'react-redux';
-import store from './redux/store';
+import { Provider as ReduxProvider } from "react-redux";
+import store from "./redux/store";
 
 // Components
-import App from './App';
+import App from "./App";
 
 // CSS
-import './predefined.css';
-import './reset.css';
-import './index.scss';
+import "./predefined.css";
+import "./reset.css";
+import "./index.scss";
 
 ReactDOM.render(
 	<ReduxProvider store={store}>
-		<BrowserRouter>
+		<BrowserRouter basename="/stepper">
 			<App />
 		</BrowserRouter>
 	</ReduxProvider>,
-	document.getElementById('root')
+	document.getElementById("root")
 );
